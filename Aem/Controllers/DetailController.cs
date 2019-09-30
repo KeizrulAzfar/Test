28bcf01cb0b74ad38934d4c9459edd76e5c139e9
KeizrulAzfar <33506257+KeizrulAzfar@users.noreply.github.com>
2019-09-30 11:35:06 +0800

    Add files via upload

---
+++ Aem/Controllers/DetailController.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Aem.Model;
+
+namespace Aem.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DetailController : ControllerBase
+    {
+        private readonly Main _context;
+
+        public DetailController(Main context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Detail
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Detail>>> GetDetails()
+        {
+            return await _context.Details.ToListAsync();
+        }
+
+        // GET: api/Detail/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Detail>> GetDetail(int id)
+        {
+            var detail = await _context.Details.FindAsync(id);
+
+            if (detail == null)
+            {
+                return NotFound();
+            }
+
+            return detail;
+        }
+
+        // PUT: api/Detail/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutDetail(int id, Detail detail)
+        {
+            if (id != detail.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(detail).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!DetailExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Detail
+        [HttpPost]
+        public async Task<ActionResult<Detail>> PostDetail(Detail detail)
+        {
+            _context.Details.Add(detail);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetDetail", new { id = detail.Id }, detail);
+        }
+
+        // DELETE: api/Detail/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Detail>> DeleteDetail(int id)
+        {
+            var detail = await _context.Details.FindAsync(id);
+            if (detail == null)
+            {
+                return NotFound();
+            }
+
+            _context.Details.Remove(detail);
+            await _context.SaveChangesAsync();
+
+            return detail;
+        }
+
+        private bool DetailExists(int id)
+        {
+            return _context.Details.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ Aem/Model/Detail.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Aem.Model
+{
+    public class Detail
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [Column(TypeName="nvarchar(60)")]
+        public string PlatformName { get; set; }
+        [Required]
+        [Column(TypeName = "nvarchar(20)")]
+        public string PlatformId { get; set; }
+        [Required]
+        [Column(TypeName = "nvarchar(60)")]
+        public string UniqueName { get; set; }
+        [Required]        
+        public string UpdatedAt { get; set; }
+        [Required]
+        public String CreatedAt { get; set; }
+
+    }
+}
